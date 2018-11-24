b03f8af1ff64fda30383f9848c5e9cf121f23d0c
Vikas Ramaneti <victor@tvally.in>
2018-11-24 13:23:36 -0800

    Modified user models and user post models. Integrated mysql database with API layer

---
+++ webapp/api/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import * 
+
+admin.site.register(User)
+admin.site.register(UserFollower)
+admin.site.register(UserImage)
+admin.site.register(PostType)
+admin.site.register(Post)
+admin.site.register(PostReceipient)
+admin.site.register(PostFile)
+admin.site.register(PostLike)
+admin.site.register(PostComment)
