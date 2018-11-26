9cdbca8a8792835cb4c333f403a9538ac55b1fba
Vikas Ramaneti <victor@tvally.in>
2018-11-25 16:09:57 -0800

    Written web views for pre login pages

--- reliquishnow/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('users/', views.UserList.as_view()),
-    path('users/<int:user_id>/', views.UserDetail.as_view()),
-    path('users/<int:user_id>/followers/', views.UserFollowersList.as_view()),
-    path('users/<int:user_id>/following/', views.UserFollowersList.as_view())
-]
